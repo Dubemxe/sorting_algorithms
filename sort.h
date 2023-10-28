cc7dcb2077fffd5883123a140e0a82932ab683c4
Chidubem Uchendu <dubemxe@gmail.com>
2023-10-28 03:55:59 -0700

    counting sort

--- sort.h
+++ sort.h
@@ -1,7 +1,24 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifndef SORT_H
 #define SORT_H
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /* Structure for doubly linked list */
 
@@ -31,4 +48,9 @@ int lomuto_partition(int *array, size_t size, int high, int low);
 void lomuto_sort(int *array, size_t size, int low, int high);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+int get_max_value(int *array, int size);
+void counting_sort(int *array, size_t size);
 #endif
